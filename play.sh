b01870ffc09a70fdf1868d2636839decd4d95779
chrisih <chrisih.is.herter@googlemail.com>
2021-10-04 16:36:13 +0200

    Update play.sh

--- play.sh
+++ play.sh
@@ -1,3 +1,3 @@
 sudo rm /usr/lib/arm-linux-gnueabihf/vlc/lua/playlist/youtube.luac 
 sudo wget https://raw.githubusercontent.com/videolan/vlc/master/share/lua/playlist/youtube.lua -O /usr/lib/arm-linux-gnueabihf/vlc/lua/playlist/youtube.lua
-pwsh ~/youtube.ps1
+~/powershell/pwsh ~/youtube.ps1
