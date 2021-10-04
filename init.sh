0040ff0e3f05f7c3769c0fb85da60df8a1643780
chrisih <chrisih.is.herter@googlemail.com>
2021-10-04 15:19:34 +0200

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,6 @@
+sudo apt install xserver-xorg
+sudo apt install vlc
+sudo apt install libssl1.0.2
+wget https://github.com/PowerShell/PowerShell/releases/download/v7.0.1/powershell-7.0.1-linux-arm32.tar.gz
+mkdir ~/powershell
+tar -xvf ./powershell-7.0.1-linux-arm32.tar.gz -C ~/powershell
