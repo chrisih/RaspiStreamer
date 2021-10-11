4765bd92c5868907bbf9857b9b8d7559b4a33836
chrisih <chrisih.is.herter@googlemail.com>
2021-10-11 07:20:29 +0200

    Update play.sh

--- play.sh
+++ play.sh
@@ -1,3 +1,4 @@
+sudo apt install streamlink
 sudo rm /usr/lib/arm-linux-gnueabihf/vlc/lua/playlist/youtube.luac 
 sudo wget https://raw.githubusercontent.com/videolan/vlc/master/share/lua/playlist/youtube.lua -O /usr/lib/arm-linux-gnueabihf/vlc/lua/playlist/youtube.lua
 /home/pi/powershell/pwsh /home/pi/youtube.ps1
